66447468947f2e0590cad2131243deecd86fa591
aartigit203 <agrawal.aarti@gmail.com>
2026-04-11 18:02:55 +0530

    Update streak_service.py

--- services/streak_service.py
+++ services/streak_service.py
@@ -4,21 +4,28 @@ from datetime import datetime
 FILE = "data/streak.json"
 
 def update_streak(user):
-    data = load_json(FILE)
-    today = str(datetime.now().date())
+    data = load_json("streak.json")
+
+    today = datetime.date.today()
+    today_str = str(today)
 
     if user not in data:
-        data[user] = {"count": 1, "last": today}
+        data[user] = {"count": 1, "last": today_str}
     else:
-        last = data[user]["last"]
-        diff = (datetime.now().date() - datetime.fromisoformat(last).date()).days
+        last_date = datetime.date.fromisoformat(data[user]["last"])
+        diff = (today - last_date).days
 
-        if diff == 1:
-            data[user]["count"] += 1
-        elif diff > 1:
-            data[user]["count"] = 1
+        if diff == 0:
+            pass  # already counted today
 
-        data[user]["last"] = today
+        elif diff == 1:
+            data[user]["count"] += 1  # continue streak
 
-    save_json(FILE, data)
+        else:
+            data[user]["count"] = 1  # reset streak gently
+
+        data[user]["last"] = today_str
+
+    save_json("streak.json", data)
+    
     return data[user]["count"]
