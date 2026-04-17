496d3fddde812f28530e7ae3e9b2ea60d52df251
aartigit203 <agrawal.aarti@gmail.com>
2026-04-17 12:18:02 +0530

    Update whatsapp.py

--- services/whatsapp.py
+++ services/whatsapp.py
@@ -34,7 +34,7 @@ def send_message(to, text):
     url = f"https://graph.facebook.com/v18.0/{PHONE_ID}/messages"
 
     headers = {
-        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Authorization": f"Bearer {TOKEN}",
         "Content-Type": "application/json"
     }
 
@@ -51,7 +51,7 @@ def send_buttons(to, text, buttons):
     url = f"https://graph.facebook.com/v18.0/{PHONE_ID}/messages"
 
     headers = {
-        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Authorization": f"Bearer {TOKEN}",
         "Content-Type": "application/json"
     }
 
